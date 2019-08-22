8973a2e4adf095dc473b4c6910ca5be4445ef777
Yehia <Yehiaass1997@gmial.com>
2019-08-22 15:08:29 +0300

    add page crud of course #9 [X]create course model [X]create validation request  for course

--- app/Course.php
+++ app/Course.php
@@ -8,11 +8,16 @@ use Illuminate\Database\Eloquent\SoftDeletes;
 class Course extends Model
 {
     use SoftDeletes;
-    protected $fillable=['title_ar','title_en','description_ar','category_id',
+    protected $fillable=['user_id','title_ar','title_en','description_en','description_ar','category_id',
         'status','is_paid','price'];
 
     public function image()
     {
             return $this->morphOne(File::class,'fileable');
     }//end of get image course
+
+     public function user()
+    {
+            return $this->belongsTo(User::class);
+    }//end of get user that have a course
 }

7c1db6e2f3aede673defceac3a7ca65efc9fc149
aliwael <dev.aliwaell@gmail.com>
2019-08-24 10:59:03 -0700

    error file lang

---
+++ resources/lang/en/error.php
@@ -0,0 +1,19 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Authentication Language Lines
+    |--------------------------------------------------------------------------
+    |
+    | The following language lines are used during authentication for various
+    | messages that we need to display to the user. You are free to modify
+    | these language lines according to your application's requirements.
+    |
+    */
+
+   'added_successfully' => 'Data added successfully',
+    'updated_successfully' => 'Data successfully modified',
+    'deleted_successfully' => 'Data successfully deleted',
+];
